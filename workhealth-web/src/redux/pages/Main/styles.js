3c0bee61635278080ce959c89ff129ca2752e45d
johnhiggs <john.ahigian@hqo.co>
2020-04-30 21:40:50 -0400

    merge

--- workhealth-web/src/redux/pages/Main/styles.js
+++ workhealth-web/src/redux/pages/Main/styles.js
@@ -105,7 +105,7 @@ export const styles = () => ({
         fontWeight: 'bold' 
     },
     dialogTitleText: {
-        color: '#027FFF'
+        color: '#fefefe'
     },
     downloadButton: {
         color: 'white',
